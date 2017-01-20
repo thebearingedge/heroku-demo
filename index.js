6806fc79e2650799e8d4e3a8641a2c09fd5d1186
thebearingedge <contact@timdav.is>
2017-01-20 14:22:32 -0800

    Adds events route.

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 var express = require('express')
+var bodyParser = require('body-parser')
 
 var app = express()
 
+var events = []
+
 app.get('/', function (req, res) {
-  res.send('hello, heroku')
+  res.json(events)
+})
+
+app.post('/events', function (req, res) {
+  events.push(req.body)
+  res.sendStatus(202)
 })
 
 app.listen(process.env.PORT)
